fc2e842514dd2cce502b0fce01155012cc81cc29
Akshay M <akshayknr7@gmail.com>
2021-01-16 15:17:01 +0530

    crypto basics done

--- crypt.py
+++ crypt.py
@@ -1,5 +1,7 @@
 from Crypto.Cipher import DES3
 import hashlib
+import random
+import constants
 
 def pad(text):
     return text + (b' ' * (len(text) % 8))
@@ -11,7 +13,7 @@ def desEncrypt(text, key):
         print("Error: " + str(e))
         return
 
-    padded_text = pad(text1)
+    padded_text = pad(text)
     encrypted_text = des.encrypt(padded_text)
     return encrypted_text
 
@@ -23,29 +25,5 @@ def sha(text):
     sha_signature = hashlib.sha256(text).hexdigest()
     return sha_signature
 
-def diffie(base, exp, prime):
-    return int(pow(base, exp, prime))
-
-def egs():
-    #Diffie example
-    # Both the persons will be agree upon the public keys G and P (where P = prime)
-    # P = 23
-    # G = 9
-    # # Alice will choose the private key a 
-    # a = 4
-    # # gets the generated key
-    # x = diffie(G,a,P)  
-    # # Bob will choose the private key b
-    # b = 3
-    # # gets the generated key
-    # y = diffie(G,b,P)
-    # # Secret key for Alice 
-    # ka = diffie(y,a,P)
-    # # Secret key for Bob 
-    # kb = diffie(x,b,P)
-    # print(ka)
-    # print(kb)
-    return
-
-key = b'hello123987654329999777'
-text1 = b'zzzaa!'
+def diffie(base, generator = constants.DIFFIE_GENERATOR, prime = constants.DIFFIE_PRIME):
+    return int(pow(base, generator, prime))
